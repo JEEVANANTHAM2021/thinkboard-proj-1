22eecccc01247b1b47a5088be05cd89e6347799f
JEEVANANTHAM2021 <jeevanathamjeeva64@gmail.com>
2026-01-11 14:51:10 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Route, Routes } from 'react-router'
+import HomePage from './Pages/HomePage'
+import CreatePage from './Pages/CreatePage'
+import NoteDetailPage from './Pages/NoteDetailPage'
+
+const App = () => {
+  return (
+    <div className='relative h-full w-full'>
+      <div className='absolute inset-0 -z-10 h-full w-full items-center px-5 py-24'
+      style={{background: "radial-gradient(125% 125% at 50% 10%, #000 55%, #00FF9D40 100%)",}} />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/create' element={<CreatePage />} />
+        <Route path='/note/:id' element={<NoteDetailPage />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
